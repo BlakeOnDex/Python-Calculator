3aa3473df45f07aed623e99ae4423d62c212f444
Blake <108043930+BlakeOnDex@users.noreply.github.com>
2023-12-23 05:53:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import time
+# print("I love black people")
+# blake = 14
+# print(blake)
+# if blake is 0:
+#     print("blake is 0")
+# elif blake is 3:
+#     print("blake is 3")
+# else:
+#     print("idk what blake is")
+
+# answer = input("What is that favorite color man ") 
+# print(answer)
+# while True:
+#     password = input("Welcome to my first python password game!")
+#     if password != "blackops1337":
+#         print("wrong")
+#     else:
+#         print("congrats on winning")
+#         time.sleep (3)
+#         exit(0)
+x=float(input("Enter First Number:"))
+y=float(input("Enter Second Number"))
+operation =input("Enter Operation")
+
+if operation == "+":
+     print(x + y)
+elif operation == "-":
+     print(x-y)
+elif operation == "*":
+     print(x*y)
+elif operation == "/":
+     print(x/y)
+else:
+     print("incorrect symbol")
+
+
+
+
+
+#     print("incorrect symbol")
+# input()
+# email = yagmail.SMTP("t0031692@gmail.com","satk afpv gxpn ldjc")
+# while True:
+#     email.send("t0031692@gmail.com","","Flood")
