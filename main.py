e2241a9a848d50157656c24d753538898bd16783
Blake <108043930+BlakeOnDex@users.noreply.github.com>
2023-12-23 05:54:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 import time
-# print("I love black people")
-# blake = 14
-# print(blake)
-# if blake is 0:
-#     print("blake is 0")
-# elif blake is 3:
-#     print("blake is 3")
-# else:
-#     print("idk what blake is")
-
-# answer = input("What is that favorite color man ") 
-# print(answer)
-# while True:
-#     password = input("Welcome to my first python password game!")
-#     if password != "blackops1337":
-#         print("wrong")
-#     else:
-#         print("congrats on winning")
-#         time.sleep (3)
-#         exit(0)
 x=float(input("Enter First Number:"))
 y=float(input("Enter Second Number"))
 operation =input("Enter Operation")
@@ -33,13 +13,3 @@ elif operation == "/":
      print(x/y)
 else:
      print("incorrect symbol")
-
-
-
-
-
-#     print("incorrect symbol")
-# input()
-# email = yagmail.SMTP("t0031692@gmail.com","satk afpv gxpn ldjc")
-# while True:
-#     email.send("t0031692@gmail.com","","Flood")
